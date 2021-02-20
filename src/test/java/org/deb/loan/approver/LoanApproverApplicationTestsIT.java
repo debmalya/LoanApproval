37beb973b2e8a9282381fb13291a95bb02645213
Debmalya Jash <debmalya.jash@gmail.com>
2021-02-20 10:05:48 +0800

    Added ControllerAdvice for error handling

--- src/test/java/org/deb/loan/approver/LoanApproverApplicationTestsIT.java
+++ src/test/java/org/deb/loan/approver/LoanApproverApplicationTestsIT.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class LoanApproverApplicationTests {
+class LoanApproverApplicationTestsIT {
   @Autowired private ApplicationContext applicationContext;
 
   @Test
